b68a0c8bf55a7064529249d4aa2863071418306d
HarrisonNoland <18nolandh36@millshighsch.org>
2017-08-24 09:34:59 -0700

    Third time's a charm

---
+++ Project2S/src/Project2S/Project2S.java
@@ -0,0 +1,13 @@
+//Harrison Noland 
+//August 24,2017
+package Project2S;
+
+public class Project2S {
+
+	public static void main(String[] args) {
+	System.out.print("Aloha");
+	System.out.println(" from Hawaii");
+	System.out.print("soup");
+	}
+
+}
