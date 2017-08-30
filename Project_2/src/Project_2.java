eb8ab919bbba338d14436e9fa165e1a987c12125
Harrison Noland <18nolandh36@millshighsch.org>
2017-08-30 10:46:16 -0700

    Hi

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+//Harrison Noland 8/23/17 
+//Project 2
+// Use println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there");
+		System.out.print("I am a turtle");
+	}
+
+}
