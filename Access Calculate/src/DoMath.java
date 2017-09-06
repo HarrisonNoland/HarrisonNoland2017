05e51da1ba916d145567f978f7ee7e2ac0bb673d
Harrison Noland <18nolandh36@millshighsch.org>
2017-09-06 10:47:27 -0700

    finish

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,15 @@
+//*Harrison Noland
+ // September 6, 2017
+ //This is the runner for the caluclate library it is used to test the methods
+public class DoMath {
+
+	public static void main(String[] args) {
+		Calculate.square(5);
+		System.out.println(Calculate.square(5));
+		Calculate.cube(4);
+		System.out.println(Calculate.cube(4));
+		Calculate.average();
+		System.out.println(Calculate.average();
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,18 @@
+//Harrison Noland
+ //September 6, 2017
+ //Self written math library
+public class Calculate {
+
+	 public static int square(int operand) {
+		 int square = operand*operand;
+		 return square;
+	 } 
+	 public static int cube(int operand) {
+		 int cube = operand*operand*operand;
+		 return cube;
+	 }
+	 public static double average(double operand) {
+		 double average = ()/2;
+		 return average;
+	 }
+}
