6a2107b7dd9a49f1fd5b783a812cb382c3c69658
Harrison Noland <18nolandh36@millshighsch.org>
2017-09-07 09:50:34 -0700

    fix errors

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -10,8 +10,12 @@ public class DoMath {
 		System.out.println(Calculate.cube(4));
 		Calculate.average(4.0,6.0);
 		System.out.println(Calculate.average(4.0,6.0));
-		Calculate.average1(5.0,9.0,120.0);
-		System.out.println(Calculate.average1(5.0,9.0,120.0));
+		Calculate.average(5.0,9.0,120.0);
+		System.out.println(Calculate.average(5.0,9.0,120.0));
+		Calculate.toDegrees(30);
+		System.out.println(Calculate.toDegrees(30));
+		Calculate.toRadians(40);
+		System.out.println(Calculate.toRadians(40));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -2,26 +2,39 @@
  //September 6, 2017
  //Self written math library
 public class Calculate {
-
+//square of a number
 	 public static int square(int operand) {
 		 int square = operand*operand;
 		 return square;
 	 } 
+	 //cube of a number
 	 public static int cube(int operand) {
 		 int cube = operand*operand*operand;
 		 return cube;
 	 }
+	 //find average of two numbers
 	 public static double average(double operand, double operand1) {
 		 double average = (operand+operand1)/2;
 		 return average;
 	 }
-	 public static double average1(double operand, double operand1, double operand2) {
+	//find average of three numbers
+	 public static double average(double operand, double operand1, double operand2) {
 	 	double average = (operand+operand1+operand2)/3;
 	 			return average;
 	 }
-	 public static double toDegrees(double operand, double operand1) {
-		 double toDegrees = 0;
-		 return 0;
+	 //convert radians to degrees
+	 public static double toDegrees(double operand) {
+		 double toDegrees = operand*(180/3.14159);
+		 return toDegrees;
+ }
+	 //convert degrees to radians
+	 public static double toRadians(double operand) {
+		 double toRadians = operand/(180*3.14159);
+		 return toRadians;
+	 }
+	 public static double discriminant(double operand,double ,double operand) {
+		double discriminant = (integer1*integer)-(4*integer*integer);
+		return discriminant;
 	 }
 	 
 }
