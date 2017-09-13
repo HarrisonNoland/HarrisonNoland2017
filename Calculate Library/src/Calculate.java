43da01d9ae71e740adc65d8cb88ad9580a16f833
Harrison Noland <18nolandh36@millshighsch.org>
2017-09-13 09:44:24 -0700

    Fix errors, add

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -32,12 +32,81 @@ public class Calculate {
 		 double toRadians = operand/(180*3.14159);
 		 return toRadians;
 	 }
+	 //Discriminant provides coefficients of a quadratic equation
 	 public static double discriminant(double integer1,double integer2 ,double integer3) {
 		double discriminant = (integer2*integer2)-(4*integer1*integer3);
 		return discriminant;
 		}
-	 public static String toImproperFrac(int wholenumber,int numerator,int denominator) {
-		 int  = wholenumber*denominator+numerator;
-		 return "/"+denominator);
+	 //toImproperFrac converts a mixed number into an improper fraction
+	 public static String toImproperFrac(int a,int b,int c) {
+		 int  = Wholenumber*Denominator+Numerator;
+		 return "/"+Denominator);
 	 }
+	 //toMixedNum converts an improper fraction into a mixed number
+	public static String toMixedNum(int Numerator, int Denominator) {
+		int wholenumber = Numerator/Denominator;
+		int nnumerator = Numerator%Denominator;
+		return (wholenumber+"_"+nnumerator+"/"+Denominator);
 }
+	//foil converts a binomial multiplication into a quadratic equation
+	public static String foil(int a, int b, int c, int d) {
+		int first = a*c;
+		int second = (a*d)+(b*c);
+		int third = c*d;
+		return (first+"x^2"+"+"+second+"x"+"+"+third);
+	}
+	//isDivisibleBy determines whether or not one integer is evenly divisible by another
+		public static boolean isDivisibleBy(int a, int b) {
+			if(a % b == 0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+		//absValue returns the absolute value of the number passed
+		public static double absValue(double a){
+			double x = a - 0;
+			if(x > 0){
+				return x;
+			}else{ 
+				return 0 - a;
+			}
+		}
+		//max returns the larger of the values passed
+		public static double max(double a, double b){
+			if(a > b){
+				return a;
+			}else{
+				return b;
+			}
+		}
+		//max overload the max method
+		public static double max(double a, double b, double c){
+			if (a > b && a > c){
+				return a;
+			}
+			if(b > a && b > c){
+				return b;
+			}
+			if(c > a && c > b){
+				return c;
+			}
+			return(2.4);
+		}
+		//min returns the smaller value passed
+		public static int min(int a, int b){
+			if(a < b){
+				return a;
+			}
+			if(b < a){
+				return b;
+			}
+			return(2);
+		}
+		//round2 rounds a double correctly to 2 decimal places
+		public static double round2(double a){
+			a = a * 100;
+			a += .5;
+			return(a / 100);
+			}
+		}
