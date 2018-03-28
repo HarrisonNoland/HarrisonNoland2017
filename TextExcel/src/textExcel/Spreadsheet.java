c9c22cba5c7f182804e9eec1f1edae907e76337d
Harrison Noland <18nolandh36@millshighsch.org>
2018-03-28 11:30:53 -0700

    i Am

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -39,10 +39,33 @@ public class Spreadsheet implements Grid {
 		}
 
 		@Override
-		public String getGridText ()
-		{
-			// TODO Auto-generated method stub
-			return null;
+		
+		public String getGridText (){
+			String product = "";
+			product += "   |";
+		for(int i = 65; i<=76; i++) {
+				product +=(char)i;
+			for (int j=0;j<9;j++) {
+					product += "";
+			}
+			product += "|";
+			}// TODO Auto-generated method stub
+			product +="/n";
+		
+			for(int d=1;d<20;d++) {
+			if (d <10) { 
+				product += d+"  ";
+		}else {
+			product += d+" ";
 		}
-
-	}
+		product +="|";	
+		}
+		for(int l=0;l<12;l++) {
+			product+=boxes [d-1][l].abbreviatedCellText();
+			product+="|";	
+		} 
+			product+="/n";
+			
+		return product;
+		}		
+}
