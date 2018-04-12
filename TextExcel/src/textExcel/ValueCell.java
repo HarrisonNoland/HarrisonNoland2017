93c4601e56caf47d5cc5eb3fe891e47d0ebd893d
HarrisonNoland <31107080+HarrisonNoland@users.noreply.github.com>
2018-04-12 14:01:03 -0700

    Update ValueCell.java

--- TextExcel/src/textExcel/ValueCell.java
+++ TextExcel/src/textExcel/ValueCell.java
@@ -1,7 +1,17 @@
 package textExcel;
 
-	public class ValueCell {
-		private String text;
-		public ValueCell(String cellText);
-		
+public class ValueCell extends RealCell{
+	
+	private String value;
+	
+	public ValueCell(String num){	// num set to value and in RealCell
+		this.value = num;
+		setRealCell(num);
+
+	}
+	
+	public double getDoubleValue(){
+		return Double.parseDouble(getRealCell());
+	}
+
 }
